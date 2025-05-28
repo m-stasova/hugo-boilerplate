c70cbab3d7da823aa6e83149afe5542eb924bbb4
vzeman <vzeman@qualityunit.com>
2025-05-28 08:34:17 +0200

    lazyvideo implementation for mp4 files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 // Import lazy loading functionality
 import './lazy-loading.js';
+// Import lazy video functionality
+import './lazy-videos.js';
 // Import banner dismiss functionality
 import './banner-dismiss.js';
 // Import cookie consent functionality
